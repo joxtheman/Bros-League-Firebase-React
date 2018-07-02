c489c1eb5fbfb5f54e5775a320f9d89c179db94e
Jovan Novakovic <zmaj.joca@gmail.com>
2018-07-03 01:06:16 +0200

    Testing live

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>BRO, SAMO DA JAVIM DA SMO LIVE</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
