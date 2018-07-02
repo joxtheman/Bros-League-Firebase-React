fdb6fd672ae26c5d749a80011a22927bd5cede77
Jovan Novakovic <zmaj.joca@gmail.com>
2018-07-03 01:47:21 +0200

    TestingWithoutWorkers

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("app"));
-registerServiceWorker();
+//registerServiceWorker();
