9212529b7fafcb7c7d2fe8d01b34160997055f84
Jovan <zmaj.joca@gmail.com>
2018-07-12 04:21:25 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,19 +9,6 @@ class Home extends Component {
     return (
       <div>
         <div className="homeIntro">
-          <p>
-            Soccer is more than sport.<span> IT'S PASSION.</span>
-          </p>
-          <p>
-            You don't watch soccer.<span> YOU LIVE IT</span>
-          </p>
-          <p>
-            Soccer is a <span>GAME:</span>
-          </p>
-          <p>
-            of <span> STRATEGY, STRENGT</span> and<span> SKILS.</span>
-          </p>
-
           <div className="doYouDare">
             <Link to="/portal">
               <Button inverted color="red">
