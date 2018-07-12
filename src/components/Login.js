eae82784ff77dadda52792cfd781b11eec2bc651
Jovan <zmaj.joca@gmail.com>
2018-07-12 04:22:57 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,43 +7,6 @@ class Login extends Component {
   render() {
     return (
       <div className="main">
-        {/* <Segment inverted> */}
-        <div className="login">
-          <h1>Portal Login</h1>
-          <Form>
-            <Form.Field>
-              <label>Email</label>
-              <input type="text" placeholder="Email" required=" " />
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <input
-                type="password"
-                className="password"
-                placeholder="Password"
-                required=" "
-              />
-            </Form.Field>
-
-            <Button basic inverted color="yellow" type="submit">
-              Login
-            </Button>
-            <div className="messages_below_form">
-              <div>
-                <Link to="#">
-                  <p>Forgot your password? Click here</p>
-                </Link>
-              </div>
-              {/* <div>
-                <Link to="/signup">
-                  <p>Create an account</p>
-                </Link>
-              </div> */}
-            </div>
-          </Form>
-        </div>
-
-        {/* </Segment> */}
       </div>
     );
   }
