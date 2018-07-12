71f6614291a5c03252732299a87df9d3f142f60d
Jovan <zmaj.joca@gmail.com>
2018-07-12 04:18:58 +0200

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyD0nfJMhKWs9i5nOVeogwX5G1Exg0gwgWE",
-  authDomain: "brosleague-d697b.firebaseapp.com",
-  databaseURL: "https://brosleague-d697b.firebaseio.com",
-  projectId: "brosleague-d697b",
+  apiKey: "YOUR API_KEY",
+  authDomain: "FIREBASE",
+  databaseURL: "FIREBASE_DATABASE_URL",
+  projectId: "PROJECT_ID",
   storageBucket: "",
-  messagingSenderId: "235430621421"
+  messagingSenderId: ""
 };
 const fire = firebase.initializeApp(config);
 export default fire;
